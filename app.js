b1c7b19de4e4edf809f5081102ea13523d2faf18
Muhammad Mudabbir <106159197+MuhammadMudabbirMunaf@users.noreply.github.com>
2022-08-11 16:00:39 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,332 @@
+let colors = ["aqua", "purple", "chocolate", "crimson", "deeppink", "green", "yellow"];
+let balloonArray = ["balloon1", "balloon2", "balloon3", "balloon4", "balloon5", "balloon6", "balloon7", "balloon8",
+            "balloon9", "balloon10", "balloon11", "balloon12", "balloon13", "balloon14", "balloon15", "balloon16",
+            "balloon17", "balloon18", "balloon19", "balloon20", "balloon21", "balloon22", "balloon23", "balloon24",
+            "balloon25", "balloon26", "balloon27", "balloon28", "balloon29", "balloon30", "balloon31", "balloon32",
+            "balloon33", "balloon34", "balloon35"];
+
+var findColor = colors[Math.floor(Math.random() * colors.length)]
+
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon1(i)`);
+    balloon1.style.backgroundColor = findColor;
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon2(i)`);
+    balloon2.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon3(i)`);
+    balloon3.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon4(i)`);
+    balloon4.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon5(i)`);
+    balloon5.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon6(i)`);
+    balloon6.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon7(i)`);
+    balloon7.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon8(i)`);
+    balloon8.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon9(i)`);
+    balloon9.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon10(i)`);
+    balloon10.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon11(i)`);
+    balloon11.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon12(i)`);
+    balloon12.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon13(i)`);
+    balloon13.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon14(i)`);
+    balloon14.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon15(i)`);
+    balloon15.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon16(i)`);
+    balloon16.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon17(i)`);
+    balloon17.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon18(i)`);
+    balloon18.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon19(i)`);
+    balloon19.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon20(i)`);
+    balloon20.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon21(i)`);
+    balloon21.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon22(i)`);
+    balloon22.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon23(i)`);
+    balloon23.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon24(i)`);
+    balloon24.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon25(i)`);
+    balloon25.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon26(i)`);
+    balloon26.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon27(i)`);
+    balloon27.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon28(i)`);
+    balloon28.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon29(i)`);
+    balloon29.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon30(i)`);
+    balloon30.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon31(i)`);
+    balloon31.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon32(i)`);
+    balloon32.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon33(i)`);
+    balloon33.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById(`balloon34(i)`);
+    balloon34.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+for (var i = 0; i < balloonArray.length; i++) {
+    let balloonArray = document.getElementById("balloon35(i)");
+    balloon35.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+}
+
+// var popBalloon =  function display() {
+//     document.getElementById(popBalloon).style.display = 'none';
+// }
+
+function display() {
+    let pop = document.getElementById("");
+    balloon1.style.display = 'none';
+}
+function display2() {
+    let pop = document.getElementById("");
+    balloon2.style.display = 'none';
+}
+function display3() {
+    let pop = document.getElementById("");
+    balloon3.style.display = 'none';
+}
+function display4() {
+    let pop = document.getElementById("");
+    balloon4.style.display = 'none';
+}
+function display5() {
+    let pop = document.getElementById("");
+    balloon5.style.display = 'none';
+}
+function display6() {
+    let pop = document.getElementById("");
+    balloon6.style.display = 'none';
+}
+function display7() {
+    let pop = document.getElementById("");
+    balloon7.style.display = 'none';
+}
+function display8() {
+    let pop = document.getElementById("");
+    balloon8.style.display = 'none';
+}
+function display9() {
+    let pop = document.getElementById("");
+    balloon9.style.display = 'none';
+}
+function display10() {
+    let pop = document.getElementById("");
+    balloon10.style.display = 'none';
+}
+function display11() {
+    let pop = document.getElementById("");
+    balloon11.style.display = 'none';
+}
+function display12() {
+    let pop = document.getElementById("");
+    balloon12.style.display = 'none';
+}
+
+function display13() {
+    let pop = document.getElementById("");
+    balloon13.style.display = 'none';
+}
+function display14() {
+    let pop = document.getElementById("");
+    balloon14.style.display = 'none';
+}
+function display15() {
+    let pop = document.getElementById("");
+    balloon15.style.display = 'none';
+}
+function display16() {
+    let pop = document.getElementById("");
+    balloon16.style.display = 'none';
+}
+function display17() {
+    let pop = document.getElementById("");
+    balloon17.style.display = 'none';
+}
+function display18() {
+    let pop = document.getElementById("");
+    balloon18.style.display = 'none';
+}
+function display19() {
+    let pop = document.getElementById("");
+    balloon19.style.display = 'none';
+}
+function display20() {
+    let pop = document.getElementById("");
+    balloon20.style.display = 'none';
+}
+function display21() {
+    let pop = document.getElementById("");
+    balloon21.style.display = 'none';
+}
+function display22() {
+    let pop = document.getElementById("");
+    balloon22.style.display = 'none';
+}
+function display23() {
+    let pop = document.getElementById("");
+    balloon23.style.display = 'none';
+}
+function display24() {
+    let pop = document.getElementById("");
+    balloon24.style.display = 'none';
+}
+function display25() {
+    let pop = document.getElementById("");
+    balloon25.style.display = 'none';
+}
+function display26() {
+    let pop = document.getElementById("");
+    balloon26.style.display = 'none';
+}
+function display27() {
+    let pop = document.getElementById("");
+    balloon27.style.display = 'none';
+}
+function display28() {
+    let pop = document.getElementById("");
+    balloon28.style.display = 'none';
+}
+function display29() {
+    let pop = document.getElementById("");
+    balloon29.style.display = 'none';
+}
+function display30() {
+    let pop = document.getElementById("");
+    balloon30.style.display = 'none';
+}
+function display31() {
+    let pop = document.getElementById("");
+    balloon31.style.display = 'none';
+}
+function display32() {
+    let pop = document.getElementById("");
+    balloon32.style.display = 'none';
+}
+function display33() {
+    let pop = document.getElementById("");
+    balloon33.style.display = 'none';
+}
+function display34() {
+    let pop = document.getElementById("");
+    balloon34.style.display = 'none';
+}
+function display35() {
+    let pop = document.getElementById("");
+    balloon35.style.display = 'none';
+}
+
+// giving random color to side color box i.e. colorkey
+
+let randomColor = findColor;
+for (var m = 0; m < colors.length; m++) {
+    let colorGet = document.getElementById(`colorkey(m)`);
+    colorkey.style.backgroundColor = findColor;
+}
+
+// giving text random color
+var a = findColor;
+for (var d = 0; d < colors.length; d++) {
+    let textColor = document.getElementById(`text(d)`);
+    text.style.color = findColor;
+}
+var gameLife = 3;
+var score = 0;
+var scoreGet = document.getElementById("score").innerHTML = "Score : " + score;
+
+if (pop.onclick.style.backgroundColor === colorGet.style.backgroundColor) {
+    score = score++;
+}
+else if (pop.onclick.style.backgroundColor !== colorGet.style.backgroundColor) {
+    score = score;
+    gameLife = gameLifes--;
+}
+
+if (gameLife === 0) {
+    alert("Game Over");
+}
+
+if (score === 35) {
+    alert("Congratulations! You popped all the balloons");
+}
